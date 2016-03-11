341e9cbdcf41902b574f800932418d68fc6e73e9
Mathieu Guillaume <mg@nuxeo.com>
2016-03-11 11:47:45 +0100

    Jenkins docker: update slave.jar on startup

---
+++ ansible/jenkins/roles/slave/files/20_update_slave_jar.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+if [ -f /opt/jenkins/bin/slave.jar ]; then
+    rm /opt/jenkins/bin/slave.jar
+fi
+wget -O /opt/jenkins/bin/slave.jar http://qa.nuxeo.org/jenkins/jnlpJars/slave.jar
+chown jenkins:jenkins /opt/jenkins/bin/slave.jar
+
